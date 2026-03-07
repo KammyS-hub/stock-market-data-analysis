164120f98af16f62e42e31ca740d6ef66e893211
KammyS-hub <sharmakamakshi2019@gmail.com>
2026-03-07 20:43:05 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,3 +4,11 @@ import yfinance as yf
 data = yf.download("AAPL", start="2023-01-01", end="2024-01-01")
 
 print(data.head())
+import matplotlib.pyplot as plt
+
+# Plot closing price
+data['Close'].plot(title="Apple Stock Closing Prices")
+
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
