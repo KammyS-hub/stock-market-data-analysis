f7afc82341448d805ead4238c5b61eb97102e1c5
KammyS-hub <sharmakamakshi2019@gmail.com>
2026-03-07 21:28:28 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,26 +1,27 @@
 import yfinance as yf
-
-# Download stock data
-ticker = input("Enter stock ticker: ")
-data = yf.download(ticker, start="2023-01-01", end="2024-01-01")
-
-print("\nStock Data Preview:")
-print(data[['Open', 'High', 'Low', 'Close', 'Volume']].head())
-print(data.head())
 import matplotlib.pyplot as plt
 
-# Plot closing price
-data['Close'].plot(title="Apple Stock Closing Prices")
+# Ask user for stock ticker
+ticker = input("Enter stock ticker: ")
 
+# Download stock data
+data = yf.download(ticker, start="2023-01-01", end="2024-01-01")
+
+# Show preview of important columns
+print("\nStock Data Preview:")
+print(data[['Open', 'High', 'Low', 'Close', 'Volume']].head())
+
+# Plot closing price
+data['Close'].plot(title=f"{ticker} Stock Closing Prices")
 plt.xlabel("Date")
 plt.ylabel("Price")
 plt.show()
+
 # Calculate 20-day moving average
 data['MA20'] = data['Close'].rolling(window=20).mean()
 
-# Plot closing price and moving average
-data[['Close', 'MA20']].plot(title="Apple Stock Price with 20-Day Moving Average")
-
+# Plot price and moving average
+data[['Close', 'MA20']].plot(title=f"{ticker} Stock Price with 20-Day Moving Average")
 plt.xlabel("Date")
 plt.ylabel("Price")
 plt.show()
