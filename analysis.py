30b85703bcd3a98fb0bf53de132924f90ca6bc97
KammyS-hub <sharmakamakshi2019@gmail.com>
2026-03-07 20:39:47 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,6 @@
-print("Stock market data analysis project started")
+import yfinance as yf
+
+# Download stock data
+data = yf.download("AAPL", start="2023-01-01", end="2024-01-01")
+
+print(data.head())
